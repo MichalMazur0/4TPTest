f9c2ad47ba806bbfc5af054546c3e649c6728cde
Jacek Zasadni <jacekzasadni@gmail.com>
2024-10-24 09:13:28 +0200

    dodanie formularza logowania

---
+++ src/components/LogForm.jsx
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <p></p>
+    <ul>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        
+    </ul>
+</body>
+</html>
